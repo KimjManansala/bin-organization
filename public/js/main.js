88ea40cbe65f701cfa65170e4668aed6387fbe7e
KimjManansala <kimjmanansala@gmail.com>
2019-01-29 18:08:47 -0600

    Added views and hanglebars

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,18 @@ window.MYAPP = window.MYAPP || {};
 
   document.addEventListener("DOMContentLoaded", () => {
     navbar.innerHTML = MYAPP.loadNavbar();
+    console.log('herro')
+   const form = document.getElementById('register-form')
+   form.addEventListener('submit', (evt)=>{
+    
+
+  })
 
   });
+
+
+
+   
 })()
 
 
